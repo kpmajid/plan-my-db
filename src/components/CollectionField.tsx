218650d31785949ea2214f11e02b9a46f5f105c7
kpmajid <kpmajid584@gmail.com>
2024-11-06 20:25:01 +0530

    feat(mongodb): add CollectionNode component

---
+++ src/components/CollectionField.tsx
@@ -0,0 +1,63 @@
+import { Handle, Position } from "@xyflow/react";
+import { Asterisk, Hash, Fingerprint } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { CollectionField as CollectionFieldType } from "@/types";
+
+interface CollectionFieldProps {
+  field: CollectionFieldType;
+}
+
+const CollectionField: React.FC<CollectionFieldProps> = ({ field }) => {
+  return (
+    <div
+      key={field.id}
+      className="flex items-center justify-between py-2 relative"
+    >
+      <Handle
+        type="source"
+        position={Position.Left}
+        id={`${field.id}-left`}
+        style={{
+          width: 8,
+          height: 8,
+          opacity: 1,
+        }}
+      />
+      <div className="flex items-center justify-between space-x-2 flex-grow px-3">
+        <div className="flex gap-2">
+          <span className="text-sm font-medium">{field.name}</span>
+          <div className="flex items-center space-x-1">
+            {field.isRequired && (
+              <Badge variant="outline" className="px-1">
+                <Asterisk className="h-3 w-3" />
+              </Badge>
+            )}
+            {field.isIndex && (
+              <Badge variant="outline" className="px-1">
+                <Hash className="h-3 w-3" />
+              </Badge>
+            )}
+            {field.isUnique && (
+              <Badge variant="outline" className="px-1">
+                <Fingerprint className="h-3 w-3" />
+              </Badge>
+            )}
+          </div>
+        </div>
+        <span className="text-xs text-muted-foreground">({field.type})</span>
+      </div>
+      <Handle
+        type="target"
+        position={Position.Right}
+        id={`${field.id}-right`}
+        style={{
+          width: 8,
+          height: 8,
+          opacity: 1,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CollectionField;
